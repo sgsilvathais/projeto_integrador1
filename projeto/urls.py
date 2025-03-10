7123b314ff4a8cb10dc53df8493278a461f21318
Willians Principe Fernandes <williansprincipe@gmail.com>
2025-03-09 23:47:25 -0300

    Renamed back from project to projeto.

--- projeto/urls.py
+++ projeto/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+def my_view():
+    ...
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view)
 ]
