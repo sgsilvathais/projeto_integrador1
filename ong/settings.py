0fdea1cb57ed04c4cbf4890f6b6fd8e86685ca14
filipe <frdestudos@gmail.com>
2025-04-19 14:47:53 -0300

    Ajusta settings e requirements para deploy com static files

--- ong/settings.py
+++ ong/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
@@ -154,3 +155,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CSRF_COOKIE_SECURE = True  # Garante que o cookie CSRF seja enviado apenas via HTTPS
 SESSION_COOKIE_SECURE = True  # Garante que o cookie da sessão seja enviado apenas via HTTPS
 SECURE_SSL_REDIRECT = True  # Redireciona HTTP para HTTPS
+
+# WhiteNoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
